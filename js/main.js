a1edbc1ad0c9f749295fde10e485596ef038d801
kaur0300 <35570054+kaur0300@users.noreply.github.com>
2018-04-26 11:36:31 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const $container = document.querySelector(".main .container");
+
+const $links = document.querySelectorAll("nav ul li a");
+
+const contents = {};
+
+fetch("./partials/home.html")
+    .then(function (response) {
+        return response.text();
+    })
+    .then(function (data) {
+        $container.innerHTML = data; //main content is loaded in the $continer
+    });
+
+const storeContents = function (urlVal) {
+
+    if (!contents[urlVal]) {
+
+        fetch(urlVal)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (data) {
+                contents[urlVal] = data;
+                $container.innerHTML = contents[urlVal]; //main content is loaded in the $continer
+            });
+    } else {
+        $container.innerHTML = contents[urlVal];
+    }
+};
+
+const handleClick = function (e) {
+    e.preventDefault();
+    let key = e.target.href;
+
+    storeContents(key);
+};
+
+for (let i = 0; i < $links.length; i++) {
+    $links[i].addEventListener("click", handleClick);
+}
